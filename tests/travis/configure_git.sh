67ec45ecba0c7559a915fed6bfbb3ab86ada9934
Matthieu Napoli <matthieu@mnapoli.fr>
2015-05-22 11:28:44 +0200

    #7726 Fix installation of git-lfs on Travis

--- tests/travis/configure_git.sh
+++ tests/travis/configure_git.sh
@@ -12,9 +12,3 @@ echo "Configuring git [email = $TRAVIS_COMMITTER_EMAIL, user = $TRAVIS_COMMITTER
 
 git config --global user.email "$TRAVIS_COMMITTER_EMAIL"
 git config --global user.name "$TRAVIS_COMMITTER_NAME"
-
-# Install git lfs
-# TODO: remove when Travis updates the VM (should be installed by default)
-curl -sLo - https://github.com/github/git-lfs/releases/download/v0.5.1/git-lfs-linux-amd64-0.5.1.tar.gz | tar xzvf -
-cd git-lfs-*
-sudo ./install.sh
---
+++ tests/travis/install_git_lfs.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Install git lfs
+# TODO: remove when Travis updates the VM (should be installed by default)
+curl -sLo - https://github.com/github/git-lfs/releases/download/v0.5.1/git-lfs-linux-amd64-0.5.1.tar.gz | tar xzvf -
+cd git-lfs-*
+sudo ./install.sh
+
+# Now that git-lfs is installed we update the repo to get the screenshots
+git pull
