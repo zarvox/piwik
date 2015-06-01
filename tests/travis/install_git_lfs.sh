3344eb12eb881ccc81b77ec32473b4d0a400053a
Matthieu Napoli <matthieu@mnapoli.fr>
2015-06-01 11:59:05 +0200

    Trying to fix git-lfs on Travis

--- tests/travis/install_git_lfs.sh
+++ tests/travis/install_git_lfs.sh
@@ -3,14 +3,14 @@
 # Install git lfs
 # TODO: remove when Travis updates the VM (should be installed by default)
 
+# Change the remote because git lfs doesn't support git:// URLs
+git remote set-url origin "https://$GITHUB_USER_TOKEN:@github.com/$TRAVIS_REPO_SLUG.git"
+
 curl -sLo - https://github.com/github/git-lfs/releases/download/v0.5.1/git-lfs-linux-amd64-0.5.1.tar.gz | tar xzvf -
 cd git-lfs-*
 sudo ./install.sh
 cd ..
 rm -rf git-lfs-*
-
-# Change the remote because git lfs doesn't support git:// URLs
-git remote set-url origin "https://$GITHUB_USER_TOKEN:@github.com/$TRAVIS_REPO_SLUG.git"
 
 # Now that git-lfs is installed we update the repo to download the screenshots
 # See https://github.com/github/git-lfs/issues/325
